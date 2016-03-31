79a31608a7a7562776f25cee213829d7612826fd
Heather Conley <hconley6695@gmail.com>
2016-03-30 21:05:32 -0400

    set up inital git; set up index.html to see what want rendered on page at the end; set up array of contacts

---
+++ src/js/data.js
@@ -0,0 +1,40 @@
+
+
+var Contacts = [
+	
+	{
+		fullName: 'Harry Potter',
+		email: hpotter@hogwarts.edu,
+		telephone: (404) 222-2222,
+		location: NoWhere, Scotland
+	},
+	{
+		fullName: 'Ron Weasley',
+		email: rweasley@hogwarts.edu,
+		telephone: (404) 222-2221,
+		location: NoWhere, Scotland
+
+	},
+	{
+		fullName: 'Hermione Granger',
+		email: hgranger@hogwarts.edu,
+		telephone: (404) 222-2223,
+		location: NoWhere, Scotland
+
+	},
+		{
+		fullName: 'Kingsley Shacklebolt',
+		email: kshacklebolt@ministry.gov,
+		telephone: (404) 222-2224,
+		location: Longdon, England
+
+	},
+		{
+		fullName: 'Grip Hook'
+		email: griphook@gringotts.com,
+		telephone: (404) 222-2225,
+		location: Diagon Alley, England
+
+	},
+
+]
---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+// Javascript Entry Point
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function renderList() {
+
+	ReactDOM.render(
+
+		document.querySelector('.app')
+	);
+}
+
+function renderIndividual(){
+
+	ReactDOM.render(
+
+		document.querySelector('.app')
+	);
+
+}
+
