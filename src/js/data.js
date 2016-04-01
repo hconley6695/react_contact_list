433bafe7e8c27031c296d1f24923026a91790f4a
Heather Conley <hconley6695@gmail.com>
2016-04-01 10:30:30 -0400

    resized images to squares and imported locally; got rid of padding next to images; put border radius around images to make them circles

--- src/js/data.js
+++ src/js/data.js
@@ -6,7 +6,7 @@ let harry =	{
 		email: 'hpotter@hogwarts.edu',
 		telephone: '(404) 222-2222',
 		location: 'NoWhere, Scotland',
-		url: "https://upload.wikimedia.org/wikipedia/en/4/44/HarryPotter5poster.jpg"
+		url: "./images/hp.jpg"
 	}
 
 let ron =	{
@@ -14,7 +14,7 @@ let ron =	{
 		email: 'rweasley@hogwarts.edu',
 		telephone: '(404) 222-2221',
 		location: 'NoWhere, Scotland',
-		url: "https://upload.wikimedia.org/wikipedia/en/5/5e/Ron_Weasley_poster.jpg"
+		url: "./images/rw.jpg"
 
 	}
 
@@ -23,7 +23,7 @@ let hermione =	{
 		email: 'hgranger@hogwarts.edu',
 		telephone: '(404) 222-2223',
 		location: 'NoWhere, Scotland',
-		url: "https://flavorwire.files.wordpress.com/2015/09/harry-potter-top-10-hermione-granger-moments-hermione-granger-358045.jpg"
+		url: "./images/hg.jpg"
 
 	}
 
@@ -32,7 +32,7 @@ let kingsley = 	{
 		email: 'kshacklebolt@ministry.gov',
 		telephone: '(404) 222-2224',
 		location: 'London, England',
-		url: "http://vignette2.wikia.nocookie.net/harrypotter/images/4/4e/Shacklebolt.jpg/revision/latest?cb=20070924132149"
+		url: "./images/ks.jpg"
 
 	}
 
@@ -41,7 +41,7 @@ let grip =	{
 		email: 'griphook@gringotts.com',
 		telephone: '(404) 222-2225',
 		location: 'Diagon Alley, England',
-		url: "http://vignette2.wikia.nocookie.net/es.harrypotter/images/d/d8/P7_Griphook.jpg/revision/latest?cb=20110125221109"
+		url: "./images/gh.jpg"
 
 	}
 
